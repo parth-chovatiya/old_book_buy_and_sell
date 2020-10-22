8d7a39300cc84ea1f9b7ef02ed987a1459a9ce1e
Parth Chovatiya <65696689+parth-chovatiya@users.noreply.github.com>
2020-10-22 12:47:02 +0530

    bill generated function added

--- temp1.cpp
+++ temp1.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<fstream>
 #include<cstring>
+#include<ctime>
 #include<bits/stdc++.h>
 using namespace std;
 int book_id=0;
@@ -67,6 +68,7 @@ public:
     void viewAllbooks();
     void delete_book(const char *);
     int search_book(const char *);
+    void generate_bill();
 };
 int Book :: storeBook()
 {
@@ -112,30 +114,52 @@ void Book :: delete_book(const char *t)
 }
 int Book :: search_book(const char *t)
 {
-    // Book b;
     int flag=0;
     ifstream fin;
-    // cout<<"Befour open\n";
     fin.open("file12.dat",ios::in|ios::binary);
-    // cout<<"After open\nBefour first read\n";
     fin.read((char*)this,sizeof(*this));
-    // cout<<"After first read\n";
     while(!fin.eof()){
-        // cout<<"Inside while\n";
         if(!strcmp(t,book_name)){
-            // showBookData();
-            // cout<<"Inside if\n";
             flag=1;
+            break;
         }
         fin.read((char*)this,sizeof(*this));
     }
-    // if(flag==0){
-        // cout<<"Book not found\n";
-    // }
-    // cout<<"Befour close\n";
     fin.close();
-    // cout<<"After close\n";
     return flag;
+}
+void Book :: generate_bill()
+{
+        string name;
+        cout<<"Enter your name : "; cin>>name;
+        ofstream fout;
+        time_t current = time(0);
+        char* dt = ctime(&current);
+        fout.open("file12_bill.txt",ios::out);
+        fout<<"\t\t\t\tBook Bill\n";
+        fout<<"\t\t\t\t---------\n\n";
+        fout<<"Date : "<<dt<<"\n";
+        fout<<"Customer Name : "<<name<<"\n\n\n";
+        fout<<"\t\t\tBook Details\n";
+        fout<<"\t\t\t-------------\n\n";
+        fout<<"----------------------------------------------------------------------------------------------------------------------------\n";
+        fout<<center("book_id",7)<<" | "<<center("book_isbn_number",16)<<" | "<<center("book_page",9)<<" | "<<center("book_edition",12)<<" | "<<center("book_name",25)<<" | "<<center("publisher_name",25)<<" | "<<center("book_price",10)<<" |\n";
+        fout<<"----------------------------------------------------------------------------------------------------------------------------\n";
+        fout<<my_print(book_id,7)<<" | "<<my_print(book_isbn_number,16)<<" | "<<my_print(book_page,9)<<" | "<<my_print(book_edition,12)<<" | "<<my_print(book_name,25)<<" | "<<my_print(publisher_name,25)<<" | "<<my_print(book_price,10)<<" |"<<endl;
+        fout<<"----------------------------------------------------------------------------------------------------------------------------\n";
+        fout<<"\n\n";
+        fout<<"Book Price    : "<<book_price<<"\n";
+        fout<<"Total tax(5%) : "<<book_price*0.05<<"\n";
+        fout<<"-----------------------\n";
+        fout<<"Total Price   : "<<book_price+book_price*0.05;
+        fout<<"\n\n\n";
+        fout<<"---: Terms & Conditions :---\n\n";
+        fout<<"1. You can replace your order withon 7 working days\n";
+        fout<<"2. This bill is very important when you replace order\n";
+        fout<<"3. You can't give this book to other\n";
+        fout<<"4. Damage during transist (if any) will be borne ny the supplier\n";
+        fout<<"\n\n\t\t Thank You ...";
+        fout<<"\n\t\t visit again ...\n";
 }
 int main()
 {
@@ -163,12 +187,12 @@ int main()
             cout<<"Enter Book name : ";
             cin>>s;
             if(b.search_book(s)==1){
-                b.delete_book(s);
                 cout<<"You have successfully purchase book\n";
                 char c;
                 cout<<"You want bill? y/n : ";  cin>>c;
                 if(c=='y'){
-                    generate_bill();
+                    b.generate_bill();
+                    b.delete_book(s);
                 }
                 else{
                     cout<<"Thank You ..\n";
